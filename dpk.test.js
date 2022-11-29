143e09945c7c9d9595fed78f439f9dbc5a3ab737
Benjamin Iduwe <bencoderus@gmail.com>
2022-11-29 11:27:19 +0100

    Fix indenting

--- dpk.test.js
+++ dpk.test.js
@@ -1,5 +1,5 @@
 const { deterministicPartitionKey } = require("./dpk");
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 test("when a partition key is provided, it would be used as the candidate", () => {
   const payload = {
@@ -12,95 +12,104 @@ test("when a partition key is provided, it would be used as the candidate", () =
   expect(candidate).toBe(payload.partitionKey);
 });
 
-
 test("when the partition key provided is not a string", () => {
-    const payload = {
-      name: "benjamin",
-      partitionKey: {"key": "ahahahseiei"},
-    };
-  
-    const candidate = deterministicPartitionKey(payload);
-    const result = JSON.stringify(payload.partitionKey);
-  
-    expect(candidate).toBe(result);
+  const payload = {
+    name: "benjamin",
+    partitionKey: { key: "ahahahseiei" },
+  };
 
-    const payload2 = {
-        name: "benjamin",
-        partitionKey: 111111,
-      };
-    
-      const candidate2 = deterministicPartitionKey(payload2);
-      const result2 = JSON.stringify(payload2.partitionKey);
-    
-      expect(candidate2).toBe(result2);
+  const candidate = deterministicPartitionKey(payload);
+  const result = JSON.stringify(payload.partitionKey);
 
-      const payload3 = {
-        name: "benjamin",
-        partitionKey: [1,2],
-      };
-    
-      const candidate3 = deterministicPartitionKey(payload3);
-      const result3 = JSON.stringify(payload3.partitionKey);
-    
-      expect(candidate3).toBe(result3);
+  expect(candidate).toBe(result);
 
-      const payload4 = {
-        name: "benjamin",
-        partitionKey: true,
-      };
-    
-      const candidate4 = deterministicPartitionKey(payload4);
-      const result4 = JSON.stringify(payload4.partitionKey);
-    
-      expect(candidate4).toBe(result4);
-  });
+  const payload2 = {
+    name: "benjamin",
+    partitionKey: 111111,
+  };
 
+  const candidate2 = deterministicPartitionKey(payload2);
+  const result2 = JSON.stringify(payload2.partitionKey);
+
+  expect(candidate2).toBe(result2);
+
+  const payload3 = {
+    name: "benjamin",
+    partitionKey: [1, 2],
+  };
+
+  const candidate3 = deterministicPartitionKey(payload3);
+  const result3 = JSON.stringify(payload3.partitionKey);
+
+  expect(candidate3).toBe(result3);
+
+  const payload4 = {
+    name: "benjamin",
+    partitionKey: true,
+  };
+
+  const candidate4 = deterministicPartitionKey(payload4);
+  const result4 = JSON.stringify(payload4.partitionKey);
+
+  expect(candidate4).toBe(result4);
+});
 
 test("when a partition key is not provided, it would use a sha512 hash generated using the payload", () => {
-    const payload = {
-        name: "benjamin",
-    };
+  const payload = {
+    name: "benjamin",
+  };
 
-   const hash = crypto.createHash("sha3-512").update(JSON.stringify(payload)).digest("hex")
+  const hash = crypto
+    .createHash("sha3-512")
+    .update(JSON.stringify(payload))
+    .digest("hex");
 
-    const candidate = deterministicPartitionKey(payload);
-    
-    expect(candidate).toBe(hash);
+  const candidate = deterministicPartitionKey(payload);
 
+  expect(candidate).toBe(hash);
 
-    const payload2 = {
-        name: "benjamin",
-        partitionKey: false,
-    };
+  const payload2 = {
+    name: "benjamin",
+    partitionKey: false,
+  };
 
-   const hash2 = crypto.createHash("sha3-512").update(JSON.stringify(payload2)).digest("hex")
+  const hash2 = crypto
+    .createHash("sha3-512")
+    .update(JSON.stringify(payload2))
+    .digest("hex");
 
-    const candidate2 = deterministicPartitionKey(payload2);
-    
-    expect(candidate2).toBe(hash2);
+  const candidate2 = deterministicPartitionKey(payload2);
 
-    const payload3 = {
-        name: "benjamin",
-        partitionKey: 0,
-    };
+  expect(candidate2).toBe(hash2);
 
-   const hash3 = crypto.createHash("sha3-512").update(JSON.stringify(payload3)).digest("hex")
+  const payload3 = {
+    name: "benjamin",
+    partitionKey: 0,
+  };
 
-    const candidate3 = deterministicPartitionKey(payload3);
-    
-    expect(candidate3).toBe(hash3);
-  });
+  const hash3 = crypto
+    .createHash("sha3-512")
+    .update(JSON.stringify(payload3))
+    .digest("hex");
 
+  const candidate3 = deterministicPartitionKey(payload3);
 
-  test("when a partition key provided is greater than 256 characters, it would hash the partition key and use the hashed value", () => {
-    const payload = {
-        name: "benjamin",
-        partitionKey: "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellhellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohelloohellohellohellohellohellohellohellohellohello",
-    };
+  expect(candidate3).toBe(hash3);
+});
 
-   const hash = crypto.createHash("sha3-512").update(payload.partitionKey).digest("hex");
+test("when a partition key provided is greater than 256 characters, it would hash the partition key and use the hashed value", () => {
+  const payload = {
+    name: "benjamin",
+    partitionKey:
+      "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellhellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohelloohellohellohellohellohellohellohellohellohello",
+  };
 
-    const candidate = deterministicPartitionKey(payload);
-    
-    expect(candidate).toBe(hash);
-  });
+  const hash = crypto
+    .createHash("sha3-512")
+    .update(payload.partitionKey)
+    .digest("hex");
+
+  const candidate = deterministicPartitionKey(payload);
+
+  expect(candidate).toBe(hash);
+});
